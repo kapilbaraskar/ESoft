283900eb07edd451f45f2f9954544157d88786cb
DESKTOP-SFJUN3U\admin <kbaraskar735@gmail.com>
2023-08-01 01:29:22 +0530

    no message

---
+++ Esoft/App_Start/WebApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using Esoft.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Esoft
+{
+    public static class WebApiConfig
+    {
+        public static string UrlPrefix { get { return "api"; } }
+        public static string UrlPrefixRelative { get { return "~/api"; } }
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "ApiByAction",
+                routeTemplate: "api/{controller}/{action}",
+                defaults: new { action = "Post" }
+            );
+
+            config.Filters.Add(new ApiErrorException());
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+        }
+    }
+}
